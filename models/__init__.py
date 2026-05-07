05e16cfd22751c920bf90995d63024014c12981a
Evgen Cerkun <wildhack98769@gmail.com>
2026-05-07 12:29:17 +0300

    Fix: Ignore ruff I001 for models/__init__.py due to Odoo loading order

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 from . import (
+    hr_hospital_medic_info,
     hr_hospital_disease,
     hr_hospital_doctor,
     hr_hospital_doctor_category,
     hr_hospital_doctor_history,
-    hr_hospital_medic_info,
     hr_hospital_patient,
     hr_hospital_visit,
 )
